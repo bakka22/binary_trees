64118a652c0a5cf496bba931beff9db991177232
bakka22 <abobakereltom@gmail.com>
2024-07-17 10:02:07 +0200

    s

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * binary_tree_sibling - check if the binary tree is full
+ * @tree: the binary tree
+ * Return: nothing
+*/
+binary_tree_t *binary_tree_sibling(const binary_tree_t *tree)
+{
+
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	if (tree == tree->parent->left)
+		return (tree->parent->right);
+	return (tree->parent->left);
+
+}
