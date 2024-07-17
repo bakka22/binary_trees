905da172741a6a87ccc414a2cd864e8361534099
bakka22 <abobakereltom@gmail.com>
2024-07-17 09:56:39 +0200

    s

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 #include <stddef.h>
 #include <stdlib.h>
-#include <math.h>
+#include <stdio.h>
 
 size_t binary_tree_height3(const binary_tree_t *tree);
 /**
- * binary_tree_leaves - count the number of leaves of a binary tree
+ * binary_tree_leaves2 - count the number of leaves of a binary tree
  * @tree: the binary tree
  * Return: nothing
 */
@@ -24,25 +24,31 @@ size_t binary_tree_leaves2(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
+ * binary_tree_is_perfect - check if a binary tree is perfect
  * @tree: the binary tree
  * Return: nothing
 */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t l, r;
-	int b_factor;
+	size_t height, leaves, res;
+	int i;
 
 	if (tree == NULL)
 		return (0);
-	l = binary_tree_height2(tree->left);
-	r = binary_tree_height2(tree->right);
-	b_factor = (l - r);
-	return (b_factor);
+	res = 1;
+	height = binary_tree_height3(tree);
+	leaves = binary_tree_leaves2(tree);
+	for (i = 0; i < (int)height; i++)
+	{
+		res *= 2;
+	}
+	if (leaves == res)
+		return (1);
+	return (0);
 }
 
 /**
- * binary_tree_height2 - measures the height of a binary tree
+ * binary_tree_height3 - measures the height of a binary tree
  * @tree: the binary tree
  * Return: nothing
 */
